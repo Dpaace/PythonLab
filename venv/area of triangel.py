211dcf631b1b2ab13782bfca1359dabf6d5712b5
DPAACE <dipeshnepali767@gmail.com>
2021-03-04 09:09:22 +0545

    Print types

---
+++ venv/area of triangel.py
@@ -0,0 +1,4 @@
+base_value = int(input("Enter the base of the triangle:"))
+height_value = int(input("Enter the height of the triangle:"))
+area = (base_value*height_value)//2
+print(f"The area of right angled triangle is {area}")
