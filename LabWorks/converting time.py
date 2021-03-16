6e6109cc103f254ec6baef302c0c6a59f0e2803f
DPAACE <dipeshnepali767@gmail.com>
2021-03-16 11:25:06 +0545

    new program

---
+++ LabWorks/converting time.py
@@ -0,0 +1,18 @@
+'''
+Write a program to convert seconds to day, hour, minutes and seconds.
+'''
+
+second = int(input("Enter the value for seconds :"))
+
+day = (((second // 60) // 60) // 24)
+
+hour = ((second // 60 ) // 60)
+
+minutes = (second // 60)
+
+print(f"Total day for given seconds:{day}")
+print(f"Total hour for given seconds:{hour}")
+print(f"Total minutes for given seconds:{minutes}")
+
+
+
---
+++ LabWorks/sum of first n positive numbers.py
@@ -0,0 +1,5 @@
+'''
+Write a program to find a sum of first n positive integers
+        sum = (n * (n + 1))/2
+'''
+
