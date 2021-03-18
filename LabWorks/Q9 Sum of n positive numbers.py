d5b3898e3b6573afcaa39e849d555b226d57f6ff
DPAACE <dipeshnepali767@gmail.com>
2021-03-18 09:27:13 +0545

    update

--- LabWorks/Q9 Sum of n positive numbers.py
+++ LabWorks/Q9 Sum of n positive numbers.py
@@ -3,6 +3,12 @@ Write a python program to find sum of the first n positive integers.
     sum = ( n * ( n + 1)) / 2
 '''
 
+'''
 n = int(input("Enter any number upto which you want to find the sum:"))
 sum = int(( n * ( n + 1)) / 2)
 print(f"The sum of the given positive integers is {sum}")
+'''
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = sum(list)
+print(f"The sum of the integers is {total}")
