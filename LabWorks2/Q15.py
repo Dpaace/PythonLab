76e4da5e0891eab3139222dd09415da52004b47b
DPAACE <dipeshnepali767@gmail.com>
2021-04-22 09:11:07 +0545

    Lab Exercise 2

--- LabWorks2/Q15.py
+++ LabWorks2/Q15.py
@@ -12,3 +12,30 @@
  i)b-a ==c-b
  j)b >= d
 '''
+
+a = 2
+b = 3
+c = 4
+d = 3
+
+
+#print(a == b)
+#False
+#print(a != d)
+#True
+#print(b == d)
+#True
+#print(a != c)
+#False
+#print(a += c)
+#invalid syntax
+#print(b /= d)
+#invalid syntax
+#print(b > a)
+#True
+#print(a < d)
+#True
+#print(b - a == c -b)
+#True
+#print(b >= d)
+#True
